bf361b5c03854f0b7b85db7cb97a5eb702193fc0
koreatlwls <koreatlwls@gmail.com>
2023-06-18 16:38:09 +0900

    feat : implementation mainviewmodel + mainscreen

--- WebBrowser/app/src/main/java/com/kimsijin/webbrowser/ui/MainActivity.kt
+++ WebBrowser/app/src/main/java/com/kimsijin/webbrowser/ui/MainActivity.kt
@@ -13,36 +13,62 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel = viewModel<MainViewModel>()
 
+            HomeScreen(viewModel = viewModel)
         }
     }
 }
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(viewModel: MainViewModel) {
+    val focusManager = LocalFocusManager.current
+
+    val (inputUrl, setUrl) = rememberSaveable {
+        mutableStateOf("https://www.google.com")
+    }
+
+    val scaffoldState = rememberScaffoldState()
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text("나만의 웹 브라우저") },
                 actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
+                    IconButton(
+                        onClick = {
+                            viewModel.undo()
+                        }
+                    ) {
                         Icon(
                             imageVector = Icons.Default.ArrowBack,
                             contentDescription = "back",
                             tint = Color.White
                         )
                     }
-                    IconButton(onClick = { /*TODO*/ }) {
+                    IconButton(
+                        onClick = {
+                            viewModel.redo()
+                        }
+                    ) {
                         Icon(
                             imageVector = Icons.Default.ArrowForward,
                             contentDescription = "forward",
@@ -51,7 +77,8 @@ fun HomeScreen() {
                     }
                 }
             )
-        }
+        },
+        scaffoldState = scaffoldState
     ) {
         Column(
             modifier = Modifier
@@ -59,34 +86,73 @@ fun HomeScreen() {
                 .fillMaxSize()
         ) {
             OutlinedTextField(
-                value = "",
-                onValueChange = {},
+                value = inputUrl,
+                onValueChange = setUrl,
                 label = { Text("https://") },
                 modifier = Modifier.fillMaxWidth(),
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-                keyboardActions = KeyboardActions(onSearch = {})
+                keyboardActions = KeyboardActions(onSearch = {
+                    viewModel.url.value = inputUrl
+                    focusManager.clearFocus()
+                })
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            MyWebView()
+            MyWebView(
+                viewModel,
+                scaffoldState = scaffoldState
+            )
         }
     }
 }
 
 @Composable
-fun MyWebView() {
+fun MyWebView(
+    viewModel: MainViewModel,
+    scaffoldState: ScaffoldState
+) {
+    val webView = rememberWebView()
+
+    LaunchedEffect(Unit) {
+        viewModel.undoSharedFlow.collectLatest {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            } else {
+                scaffoldState.snackbarHostState.showSnackbar("더 이상 뒤로 갈 수 없음")
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.redoSharedFlow.collectLatest {
+            if (webView.canGoForward()) {
+                webView.goForward()
+            } else {
+                scaffoldState.snackbarHostState.showSnackbar("더 이상 앞으로 갈 수 없음")
+            }
+        }
+    }
+
     AndroidView(
         modifier = Modifier.fillMaxSize(),
-        factory = {
-            WebView(it).apply {
-                settings.javaScriptEnabled = true
-                webViewClient = WebViewClient()
-                loadUrl("https://google.com")
-            }
-        },
-        update = {
-
+        factory = { webView },
+        update = { webView ->
+            webView.loadUrl(viewModel.url.value)
         }
     )
+}
+
+@Composable
+fun rememberWebView(): WebView {
+    val context = LocalContext.current
+    val webView = remember {
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl("https://google.com")
+        }
+    }
+
+    return webView
 }
