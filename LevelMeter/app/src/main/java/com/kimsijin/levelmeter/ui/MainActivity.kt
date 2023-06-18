c0cec26cc4b3b75ff9256dc1b19cbdce969838ee
koreatlwls <koreatlwls@gmail.com>
2023-06-18 20:19:46 +0900

    rename : package

--- LevelMeter/app/src/main/java/com/kimsijin/levelmeter/ui/MainActivity.kt
+++ LevelMeter/app/src/main/java/com/kimsijin/levelmeter/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.kimsijin.levelmeter
+package com.kimsijin.levelmeter.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -16,25 +16,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LevelMeterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LevelMeterTheme {
-        Greeting("Android")
-    }
-}
