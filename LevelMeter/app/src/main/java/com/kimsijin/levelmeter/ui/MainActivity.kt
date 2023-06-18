0b2ed417c4170096d0dcb8e0ab3c5326570a162c
koreatlwls <koreatlwls@gmail.com>
2023-06-18 20:38:17 +0900

    feat : mainviewmodel + mainscreen

--- LevelMeter/app/src/main/java/com/kimsijin/levelmeter/ui/MainActivity.kt
+++ LevelMeter/app/src/main/java/com/kimsijin/levelmeter/ui/MainActivity.kt
@@ -1,23 +1,74 @@
 package com.kimsijin.levelmeter.ui
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kimsijin.levelmeter.ui.theme.LevelMeterTheme
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
 
+    private val viewModel by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
+        super.onCreate(savedInstanceState)
+
+
+        lifecycle.addObserver(viewModel)
+
+        setContent {
+            TiltScreen(
+                x = viewModel.x.value,
+                y = viewModel.y.value
+            )
         }
     }
 }
 
+@Composable
+fun TiltScreen(x: Float, y: Float) {
+    val yCoord = x * 20
+    val xCoord = y * 20
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val centerX = size.width / 2
+        val centerY = size.height / 2
+
+        drawCircle(
+            color = Color.Black,
+            radius = 100f,
+            center = Offset(centerX, centerY),
+            style = Stroke(),
+        )
+
+        drawCircle(
+            color = Color.Green,
+            radius = 100f,
+            center = Offset(xCoord + centerX, yCoord + centerY),
+        )
+
+        drawLine(
+            color = Color.Black,
+            start = Offset(centerX - 20, centerY),
+            end = Offset(centerX + 20, centerY)
+        )
+
+        drawLine(
+            color = Color.Black,
+            start = Offset(centerX, centerY - 20),
+            end = Offset(centerX, centerY + 20)
+        )
+    }
+}
