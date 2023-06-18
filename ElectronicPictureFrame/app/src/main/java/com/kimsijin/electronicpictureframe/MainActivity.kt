7d53b5af74049b51b78d12f517165d71a51c9238
koreatlwls <koreatlwls@gmail.com>
2023-06-18 17:24:23 +0900

    feat : MainScreen 일부 구현

--- ElectronicPictureFrame/app/src/main/java/com/kimsijin/electronicpictureframe/MainActivity.kt
+++ ElectronicPictureFrame/app/src/main/java/com/kimsijin/electronicpictureframe/MainActivity.kt
@@ -1,25 +1,47 @@
 package com.kimsijin.electronicpictureframe
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kimsijin.electronicpictureframe.ui.theme.ElectronicPictureFrameTheme
+import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            ElectronicPictureFrameTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            var granted by remember { mutableStateOf(false) }
+
+            val launcher =
+                rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                    granted = isGranted
+                }
+
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                granted = true
+            }
+
+            if (granted) {
+                Text(text = "권한 허용 됨")
+            } else {
+                PermissionRequestScreen {
+                    launcher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
                 }
             }
         }
@@ -27,14 +49,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ElectronicPictureFrameTheme {
-        Greeting("Android")
+fun PermissionRequestScreen(onClick: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("권한이 허용되지 않았습니다")
+        Button(onClick = { onClick() }) {
+            Text("권한 요청")
+        }
     }
 }
+
