0fce8dd2ccbcc5f4dc03a81f89de5cc325a327bf
koreatlwls <koreatlwls@gmail.com>
2023-06-18 17:31:38 +0900

    feat mainviewmodel

---
+++ ElectronicPictureFrame/app/src/main/java/com/kimsijin/electronicpictureframe/ui/MainViewModel.kt
@@ -0,0 +1,43 @@
+package com.kimsijin.electronicpictureframe.ui
+
+import android.app.Application
+import android.content.ContentUris
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _photoUris = mutableStateOf(emptyList<Uri>())
+    val photoUris: State<List<Uri>> = _photoUris
+
+    fun fetchPhotos() {
+        val uris = mutableListOf<Uri>()
+
+        getApplication<Application>().contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            "${MediaStore.Images.ImageColumns.DATE_TAKEN} DESC"
+        )?.use { cursor ->
+            val idIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idIndex)
+
+                val contentUri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id,
+                )
+
+                uris.add(contentUri)
+            }
+        }
+
+        _photoUris.value = uris
+    }
+
+}
