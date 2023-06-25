c4ca99675d02fd2e2326d7b527f60ab091babeb1
koreatlwls <koreatlwls@gmail.com>
2023-06-25 17:29:32 +0900

    feat : MainActivity

--- Xylophone/app/src/main/java/com/kimsijin/xylophone/ui/MainActivity.kt
+++ Xylophone/app/src/main/java/com/kimsijin/xylophone/ui/MainActivity.kt
@@ -3,38 +3,75 @@ package com.kimsijin.xylophone.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kimsijin.xylophone.ui.theme.XylophoneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            XylophoneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun XylophoneScreen() {
+    val keys = listOf(
+        Pair("도", Color.Red),
+        Pair("레", Color(0xFFFF9800)),
+        Pair("미", Color(0xFFFFC107)),
+        Pair("파", Color(0xFF8BC34A)),
+        Pair("솔", Color(0xFF2196F3)),
+        Pair("라", Color(0xFF3F51B5)),
+        Pair("시", Color(0xFF673AB7)),
+        Pair("도", Color.Red)
+    )
+
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        keys.forEachIndexed { index, key ->
+            val padding = (index + 2) * 8
+
+            Keyboard(
+                color = key.second,
+                text = key.first,
+                modifier = Modifier.padding(vertical = padding.dp)
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    XylophoneTheme {
-        Greeting("Android")
+fun Keyboard(
+    color: Color,
+    text: String,
+    modifier: Modifier,
+) {
+    Box(
+        modifier = modifier
+            .width(50.dp)
+            .fillMaxHeight()
+            .background(color = color)
+    ) {
+        Text(
+            text = text,
+            style = TextStyle(color = Color.White, fontSize = 20.sp),
+            modifier = Modifier.align(Alignment.Center)
+        )
     }
 }
